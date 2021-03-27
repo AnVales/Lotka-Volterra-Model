3ed84875bf898769e28d03725f3a76a90edf9270
AnVales <a.vales@outlook.es>
2021-03-27 13:59:15 +0100

    Add files via upload

--- Lotka-Volterra.py
+++ Lotka-Volterra.py
@@ -38,14 +38,14 @@ def df_dt (x, t, a, b, c, d):
     return np.array([dx, dy])
 
 # Parameters:
-a = 0.15
+a = 0.1
 b = 0.02
 c = 0.3
 d = 0.01
 
 # Initial Conditions:
 x0 = 40
-y0 = 19
+y0 = 9
 initial_cond = np.array([x0, y0])
 
 # Conditions for Integration:
@@ -95,5 +95,54 @@ plt.ylim(0, y_max)
 plt.xlabel('Prey')
 plt.ylabel('Predator')
 plt.show()
+
+# As: C = a * ln(y) - b * y + c * ln(x) - d * x
+# With this equation the solution for the problem with different initial values can be calculated
+
+# New function
+def C(x, y, a, b, c, d):
+    return a * np.log(y) - b * y + c * np.log(x) - d * x
+
+# Plot: Use new function
+xy_step_C = 100
+x = np.linspace(0, x_max, xy_step_C)
+y = np.linspace(0, y_max, xy_step_C)
+xx, yy = np.meshgrid(x, y)
+constant = C(xx, yy, a, b, c, d)
+
+# Plot:
+plt.figure('Some solutions')
+plt.contour(xx, yy, constant, 50, cmap=plt.cm.Blues)
+plt.xlabel('Prey')
+plt.ylabel('Predator')
+plt.show()
+
+# The curves collapse at point (30, 5), this point is the center
+# This point is a stable point (critical point)
+# Another critical point is (0, 0), this is a chair point, an inestable point
+# In these point one specie can die
+
+# Plot:
+plt.figure("Critical point and some solutions")
+plt.plot(solution[:, 0], solution[:, 1], lw=2, alpha=0.8)
+plt.scatter(c/d, a/b)
+levels = (0.5, 0.6, 0.7, 0.72, 0.73, 0.74, 0.75, 0.76, 0.77, 0.775, 0.78, 0.781)
+plt.contour(xx, yy, constant, levels, colors='blue', alpha=0.3)
+plt.xlim(0, x_max)
+plt.ylim(0, y_max)
+plt.xlabel('Prey')
+plt.ylabel('Peddator')
+plt.show()
+
+# Plot:
+plt.figure('Evolution of the system over time')
+plt.plot(t, solution[:, 0], label='Prey')
+plt.plot(t, solution[:, 1], label='Predator')
+plt.legend()
+plt.xlabel('Time')
+plt.ylabel('Population')
+
+# MODIFICATION OF THE MODEL #
+# In the absence of predators, prey population growth is logistical
 
 
